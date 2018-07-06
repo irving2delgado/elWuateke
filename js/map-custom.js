22eb72a3de2b4656f0e81bad7ce3d36197805b6f
irving2delgado <36970739+irving2delgado@users.noreply.github.com>
2018-07-06 13:40:38 -0600

    google map

--- js/map-custom.js
+++ js/map-custom.js
@@ -202,8 +202,7 @@
                 map_zoom = 14;
 
             var locations = [
-                ['<div class="infobox"><h4>Pato</h4><p>Now that you visited our website, how' +
-                ' <br>about checking out our office too?</p></div>'
+                ['<div class="infobox"><h4>El Wuateke</h4></div>'
                     , latitude, longitude, 2]
             ];
 
