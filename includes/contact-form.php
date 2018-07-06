7e7eb63859901a48a7f58d0df69c223b726fd905
irving2delgado <36970739+irving2delgado@users.noreply.github.com>
2018-07-05 19:07:10 -0600

    php contact form

--- includes/contact-form.php
+++ includes/contact-form.php
@@ -10,7 +10,11 @@ if( isset($_POST['name']) && isset($_POST['email']) && isset($_POST['msg'])){
     $name = $_POST['name'];
     $email = $_POST['email'];
     $msg = nl2br($_POST['msg']);
-    $to = "irving2delgado@gmail.com";
+    if (MAIL_HOST != null) {
+        $to = MAIL_HOST;
+    } else {
+        $to = "irving2delgado@gmail.com";
+    }
     $from = $email;
     if (MAIL_TITLE != null) {
         $subject = MAIL_TITLE;
